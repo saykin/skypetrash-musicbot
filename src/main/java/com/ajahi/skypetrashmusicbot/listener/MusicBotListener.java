428426f81cda4d6330cbe0fb17ec71a61351457d
Hama Keli <saykin@gmail.com>
2022-01-16 18:20:13 +0100

    Cleaned up some sout's and sendMessages

--- src/main/java/com/ajahi/skypetrashmusicbot/listener/MusicBotListener.java
+++ src/main/java/com/ajahi/skypetrashmusicbot/listener/MusicBotListener.java
@@ -122,7 +122,7 @@ public class MusicBotListener extends ListenerAdapter {
             emoteList.add("U+33U+fe0fU+20e3");
             int count = 0;
             for (YoutubeResult results : searchResults) {
-                channel.sendMessage(results.getVideoTitle() + " " + emoteList.get(count) + " " + results.getVideoThumbnail()).queue();
+                channel.sendMessage(results.getVideoTitle() + ":one: \n" + results.getVideoThumbnail()).queue();
                 count++;
             }
 
