2b1803f68a663740087e6df65ab7481497cc1522
Hama Keli <saykin@gmail.com>
2022-01-16 17:41:36 +0100

    Moved packages and added functionality for searching youtube if no link is provided

--- src/main/java/com/ajahi/skypetrashmusicbot/listener/MusicBotListener.java
+++ src/main/java/com/ajahi/skypetrashmusicbot/listener/MusicBotListener.java
@@ -1,8 +1,10 @@
-package com.ajahi.listener;
+package com.ajahi.skypetrashmusicbot.listener;
 
 
-import com.ajahi.cleaner.MusicBotMessageCleaner;
-import com.ajahi.music.MusicBotManager;
+import com.ajahi.skypetrashmusicbot.cleaner.MusicBotMessageCleaner;
+import com.ajahi.skypetrashmusicbot.music.MusicBotManager;
+import com.ajahi.skypetrashmusicbot.youtube.YoutubeResult;
+import com.ajahi.skypetrashmusicbot.youtube.YoutubeSearch;
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
@@ -10,16 +12,14 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
-import net.dv8tion.jda.internal.entities.TextChannelImpl;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +31,7 @@ public class MusicBotListener extends ListenerAdapter {
     private final Map<Long, MusicBotManager> musicBotManagers;
     private final String musicTextChannelId;
     private MusicBotMessageCleaner cleaner;
+    private List<YoutubeResult> searchResults;
 
     public MusicBotListener() {
         this.musicBotManagers = new HashMap<>();
@@ -64,6 +65,7 @@ public class MusicBotListener extends ListenerAdapter {
         String[] command = event.getMessage().getContentRaw().split(" ", 2);
         String authorName = event.getMessage().getAuthor().getName();
         Guild guild = event.getGuild();
+
         if (command[0].equalsIgnoreCase("!play") && command.length == 2) {
             loadAndPlay(guild, authorName, event.getChannel(), command[1]);
         } else if (command[0].equalsIgnoreCase("!skip")) {
@@ -81,9 +83,55 @@ public class MusicBotListener extends ListenerAdapter {
         super.onGuildMessageReceived(event);
     }
 
+    @Override
+    public void onGuildMessageReactionAdd(@NotNull GuildMessageReactionAddEvent event) {
+        String youtubeUrl = "https://www.youtube.com/watch?v=";
+
+        if (event.getUser().isBot()) return;
+        if (!searchResults.isEmpty()) {
+            Guild guild = event.getGuild();
+            String authorName = event.getUser().getName();
+            System.out.println(authorName);
+            System.out.println(event.getReactionEmote().getAsCodepoints());
+            switch (event.getReactionEmote().getAsCodepoints()) {
+                case "U+31U+fe0fU+20e3":
+                    youtubeUrl += searchResults.get(0).getVideoId();
+                case "U+32U+fe0fU+20e3":
+                    youtubeUrl += searchResults.get(1).getVideoId();
+                case "U+33U+fe0fU+20e3":
+                    youtubeUrl += searchResults.get(2).getVideoId();
+            }
+            System.out.println(youtubeUrl);
+            loadAndPlay(guild, authorName, event.getChannel(), youtubeUrl);
+        }
+        super.onGuildMessageReactionAdd(event);
+    }
+
     private void loadAndPlay(final Guild guild, final String authorName, final TextChannel channel, final String trackUrl) {
         MusicBotManager musicManager = getBotAudioPlayer(channel.getGuild());
 
+        if (!trackUrl.matches("(.*youtube.com/watch\\?v=.*)|(.*youtu.be/.*)")) {
+            YoutubeSearch search = new YoutubeSearch();
+
+            try {
+                searchResults = search.searchYouTube(trackUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            channel.sendMessage("Top three results are as follows:").queue();
+            for (YoutubeResult results : searchResults) {
+                channel.sendMessage(results.getVideoTitle()).queue();
+                channel.sendMessage(results.getVideoThumbnail()).queue();
+            }
+
+            channel.sendMessage("Use the react buttons to choose song").queue(message -> {
+                String messageId = message.getId();
+                channel.addReactionById(messageId, "U+31U+fe0fU+20e3").queue();
+                channel.addReactionById(messageId, "U+32U+fe0fU+20e3").queue();
+                channel.addReactionById(messageId, "U+33U+fe0fU+20e3").queue();
+            });
+            return;
+        }
         playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
             @Override
             public void trackLoaded(AudioTrack track) {
@@ -102,7 +150,8 @@ public class MusicBotListener extends ListenerAdapter {
                 }
 
                 //firstTrack.getInfo().title
-                channel.sendMessage("Adding to queue " + musicManager.player.getPlayingTrack().getInfo().title + " (first track of playlist " + playlist.getName() + ")").queue();
+                channel.sendMessage("Adding to queue " + musicManager.player.getPlayingTrack().getInfo().title
+                        + " (first track of playlist " + playlist.getName() + ")").queue();
                 System.out.println(playlist.getTracks().size());
 
                 play(guild, authorName, musicManager, firstTrack);
@@ -110,14 +159,12 @@ public class MusicBotListener extends ListenerAdapter {
 
             @Override
             public void noMatches() {
-                channel.delete().queueAfter(3, TimeUnit.SECONDS);
-                channel.sendMessage("Nothing found by " + trackUrl + ", deleting message").queue();
+                channel.sendMessage("Nothing found by " + trackUrl).queue();
             }
 
             @Override
             public void loadFailed(FriendlyException exception) {
-                channel.delete().queueAfter(3, TimeUnit.SECONDS);
-                channel.sendMessage("Could not play: " + exception.getMessage() + ", deleting message").queue();
+                channel.sendMessage("Could not play: " + exception.getMessage()).queue();
             }
         });
     }
@@ -138,7 +185,11 @@ public class MusicBotListener extends ListenerAdapter {
         } catch (NullPointerException e) {
             channel.sendMessage("Nothing to skip, playlist is empty").queue();
         }
-        channel.sendMessage("Skipped to next track: " + musicManager.player.getPlayingTrack().getInfo().title).queue();
+        try {
+            channel.sendMessage("Skipped to next track: " + musicManager.player.getPlayingTrack().getInfo().title).queue();
+        } catch (NullPointerException e) {
+            channel.sendMessage("There is no next song, stopping").queue();
+        }
     }
 
     private void stopAllMusic(TextChannel channel) {
