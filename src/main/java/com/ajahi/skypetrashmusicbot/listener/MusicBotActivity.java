ab5574f4b7cf32d2d04da70b35ebfd7c963f6661
Hama Keli <8566744+saykin@users.noreply.github.com>
2022-02-19 19:57:40 +0100

    Added an activity thread to check if queue is empty and disconnect after 5 minutes.

---
+++ src/main/java/com/ajahi/skypetrashmusicbot/listener/MusicBotActivity.java
@@ -0,0 +1,46 @@
+package com.ajahi.skypetrashmusicbot.listener;
+
+import com.ajahi.skypetrashmusicbot.music.MusicBotManager;
+import net.dv8tion.jda.api.entities.Guild;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MusicBotActivity implements Runnable {
+
+    private AtomicLong timestamp;
+    private Map<Long, MusicBotManager> musicBotManagers;
+    private final Guild guildId;
+
+    public MusicBotActivity(Map<Long, MusicBotManager> musicBotManagers, Guild guildId) {
+        this.musicBotManagers = musicBotManagers;
+        this.guildId = guildId;
+    }
+
+    @Override
+    public void run() {
+        try {
+            disconnectBot();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void disconnectBot() throws InterruptedException {
+        if (isQueueEmpty()) {
+            Thread.sleep(300_000);
+            if (isQueueEmpty())
+                guildId.moveVoiceMember(Objects.requireNonNull(guildId.getMemberById("928952274972209202")), null).queue();
+            else
+                disconnectBot();
+        } else {
+            disconnectBot();
+        }
+
+    }
+
+    private boolean isQueueEmpty() {
+        return musicBotManagers.get(guildId).scheduler.isQueueEmpty();
+    }
+}
